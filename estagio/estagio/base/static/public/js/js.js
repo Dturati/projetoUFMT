6a4b0e939322c5b6661d1012ba30e25003030313
Dturati <davidturati@gmail.com>
2017-06-09 15:50:56 -0400

    primeiro commit

---
+++ estagio/estagio/base/static/public/js/js.js
@@ -0,0 +1,136 @@
+/**
+ * Created by david on 24/05/17.
+ */
+$('#id_numeroSequencial').on('keyup', function ()
+{
+    var tam = $("#id_numeroSequencial").val();
+    tam = tam.replace(/[\.-]/g, "");
+    tam = tam.replace(/[\,-]/g, "");
+    if(tam > 9999990000)
+    {
+        $("#c").val('');
+        $("#id_numeroSequencial").attr('placeholder','Valor máximo 999.999,0000');
+    }
+});
+
+
+var chamaAjax = function (elem)
+{
+
+          var caminho =  $(elem).data("dir");
+          var html = "";
+          var dadosHtml = "";
+          $.ajax({
+                type: "GET",
+                url: '/ajax/lista_diretorios/',
+                data :  {
+                  'caminho' : caminho
+                },
+                success: function (data)
+                {
+
+                    data.diretorios.forEach(function (dados)
+                    {
+                        html += '<div'+ ' '+'id='+ dados + ' '+'>';
+                            html += "&nbsp;&nbsp";
+                            html += '<a'+' ';
+                            html += 'class="glyphicon glyphicon-plus-sign"' + 'data-estado='+"fechado" + ' ' + 'onclick="chamaAjax(this)"'  + 'data-dir='+ data.caminho + '/' + dados + ' ' + 'data-toggle="collapse"' + 'data-target=""' + '>' +dados+'</a>';
+                            html += "&nbsp;&nbsp;";
+                            html += '<div' + ' ' + 'class="collapse"' + '>' + ' ';
+                            html +=  + ' '+ '</div>';
+                        html += '</div>';
+                    });
+                    $("#"+$(elem).closest("div").attr("id")).append("<br>"+html);
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    console.log(xhr.status);
+                    console.log(thrownError);
+                }
+          });
+
+};
+
+var chamaArquivosAjax = function (elem)
+{
+          var caminho =  $(elem).data("dir");
+          var html = "";
+          var dadosHtml = "";
+          var menu = "";
+          console.log('aqui');
+          $.ajax({
+                type: "GET",
+                url: '/ajax/lista_diretorios/',
+                data :  {
+                  'caminho' : caminho
+                },
+                success: function (data)
+                {
+
+                     for(var i = 0; i <data.diretorios.length; i++)
+                    {
+                            html += '<tr>';
+                            html += '<td>';
+                            html += '<a'+' ';
+                            html += 'class="glyphicon glyphicon-folder-open"' +'style="cursor:Pointer"' + '' + 'onclick="chamaArquivosAjax(this)"'  + 'data-dir='+ data.caminho + '/' + data.diretorios[i]  + '>' +' '+data.diretorios[i]+'</a>';
+                            html += '</td>';
+                            html += '<td>';
+                            html += data.detalhePastaModificadas[i];
+                            html += '</td>';
+                            html += '<td>';
+                            html += data.detalhePastaCriadas[i];
+                            html += '</td>';
+                            html += '</tr>';
+
+                    };
+                    menu += '<div class="glyphicon glyphicon-arrow-left"></div>';
+                    menu += '<a ';
+                    menu += 'onclick="chamaArquivosAjax(this)"' + '' + 'style="color:green;cursor:pointer"' + ' ' + 'data-dir=' + data.anterior + ' ' + '/' + ' ';
+                    menu += '>';
+                    menu += data.caminho;
+                    menu += '</a>';
+                    menu += "&nbsp;&nbsp;&nbsp;";
+
+                    dadosHtml += "<br>";
+
+
+                    for(var i = 0; i < data.arquivos.length; i = i+1)
+                    {
+                        console.log(data.caminho);
+                         dadosHtml += '<tr>';
+                         dadosHtml += '<td>';
+                         dadosHtml += '<div'+' ';
+                         dadosHtml += 'class="glyphicon glyphicon-file"' + 'style="color:orange;cursor:Pointer"' +' + data-caminho='+data.caminho + '/' +data.arquivos[i]+'>' + '&nbsp;&nbsp' + data.arquivos[i] +"&nbsp;";
+                         dadosHtml += '</div>'
+                         dadosHtml += '</td>';
+                         dadosHtml += '<td>';
+                         dadosHtml += data.detalheArquivosModificado[i];
+                         dadosHtml += '</td>';
+                         dadosHtml += '<td>';
+                         dadosHtml += data.detalheArquivosCriados[i];
+                         dadosHtml += '</td>';
+                         dadosHtml += '<td>';
+                         dadosHtml += '<a class = "glyphicon glyphicon-download" ';
+                         dadosHtml += ' '+'href=';
+                         dadosHtml += "'";
+                         dadosHtml += "/home/download/"+"."+data.caminho + '/'+data.arquivos[i];
+                         dadosHtml += "'";
+                         dadosHtml += ' >';
+                         dadosHtml += '</a>';
+                         dadosHtml += '</td>';
+                         dadosHtml += '</tr>';
+                    }
+                    $("#volta_dir").html(menu);
+
+                    $("#tabela").html(html + dadosHtml);
+
+
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    console.log(xhr.status);
+                    console.log(thrownError);
+                }
+          });
+};
+
+
+
---
+++ estagio/estagio/base/views.py
@@ -0,0 +1,129 @@
+from builtins import type
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import ListaArquivos,PesquisaArquivos
+from django.conf import settings
+from django.http import HttpResponse
+import os,time
+from .form import Pesquisa
+import re
+from .models import Download,Compacta_aquivos
+from wsgiref.util import FileWrapper
+import mimetypes
+import zipfile
+from io import StringIO,BytesIO
+# Create your views here.
+lista_arquivos = ListaArquivos
+
+def home(request):
+    form = Pesquisa()
+    resultadoPesquisa = []
+    detalheArquivosCriados = []
+    detalheArquivosModificado = []
+    diretorio = []
+    if request.method == 'POST':
+        form = Pesquisa(request.POST)
+        if form.is_valid():
+            resultadoPesquisa,diretorio,detalheArquivosCriados,detalheArquivosModificado = pesquisa(form.data)
+        else:
+            print("Invalido")
+    contexto = {
+        'form' : form,
+        'resultadoPesquisa' : resultadoPesquisa,
+        'diretorio' : diretorio,
+        'iterar' : range(len(resultadoPesquisa)),
+        'teste' : zip(resultadoPesquisa,diretorio,detalheArquivosModificado,detalheArquivosModificado)
+
+    }
+    return render(request,"home.html",contexto)
+
+def contatos(request):
+    return render(request,"contatos.html",{"teste":"teste"})
+
+def pesquisa(dados):
+    pesquisa_arquivos = PesquisaArquivos
+    resultado = dados['numeroSequencial']
+    resultado += '-' + dados['porcentagem']
+    resultado += '-' + dados['tipoFalha']
+    resultado += '-' + '_'+'13'+'_'
+    resultado +=  dados['tipoFalhaConjunto']
+    resultado += '-' + dados['variavelFalhada']
+    resultado += '-' + dados['metodoUtilizado']
+    resultado += '.csv'
+
+    meuDir = '/arquivos'
+    resultadoPesquisa,diretorio,detalheArquivosCriados,detalheArquivosModificado = pesquisa_arquivos.lista_aquivos(resultado)
+    return resultadoPesquisa,diretorio,detalheArquivosCriados,detalheArquivosModificado
+
+def lista_diretorios(request):
+    caminho = request.GET.get('caminho', None)
+    if(caminho == '/'):
+        caminho = '/arquivos'
+    meuDir = caminho
+    diretorio, arquivos,detalheArquivosModificado,detalheArquivosCriados,detalhePastaModificadas,detalhePastaCriadas = lista_arquivos.list_files(os.getcwd() + meuDir)
+
+    #Remove / duplicado em caminhos de diretorios
+    teste = list(caminho[::-1].split()[0])
+    anterior = list(caminho[::-1].split()[0])
+    char = teste[0]
+    if(teste[0] == '/'):
+        teste.pop(0)
+        char = teste[0]
+    cont = 0
+    while(char != '/'):
+        teste.pop(0)
+        char = teste[0]
+        if(teste[0] == '/'):
+            teste.pop(0)
+
+    teste = teste[::-1]
+    teste = ''.join(teste)
+
+
+    data = {
+        'anterior': teste,
+        'arquivos' : arquivos,
+        'diretorios' : diretorio,
+        'caminho' : meuDir,
+        'detalheArquivosModificado' : detalheArquivosModificado,
+        'detalheArquivosCriados' : detalheArquivosCriados,
+        'detalhePastaModificadas' : detalhePastaModificadas,
+        'detalhePastaCriadas' : detalhePastaCriadas
+    }
+    arquivosJson = JsonResponse(data)
+
+    return arquivosJson
+
+#Faz download de um arquivo
+def download(request,path):
+    downloadModel = Download()
+    return downloadModel.getDownload(request,path)
+
+def compacta_pesquisa(request):
+    request = request.GET.getlist('data[]')
+    arquivos = []
+    for r in request:
+        arquivos.append(str(os.getcwd() + '/' + r))
+
+    # zip_subdir = 'estagio'
+    zf = zipfile.ZipFile('pesquisa.zip', "w")
+    for fpath in request:
+        fdir, fname = os.path.split(fpath)
+        zip_subdir = str(fdir)
+        zip_path = os.path.join(zip_subdir, fname)
+        zf.write(fpath, zip_path)
+    zf.close()
+    return JsonResponse({'status':'ok'})
+
+def baixar_pesquisa(request):
+    nome_arquivo = os.getcwd() + "/" + "pesquisa.zip"
+    nome_download = "pesquisa.zip"
+    response = HttpResponse(open(nome_arquivo, 'rb').read(), content_type='x-zip-compressed')
+    response['Content-Disposition'] = "attachment; filename=%s" % nome_download
+    return response
+
+
+
+
+
+
