9b7c95b0b8c3bda838a7e2e3c31b3be900be31e0
Raphael <raphael@ic.ufmt.br>
2019-04-23 11:24:29 -0400

    Ajuste layout da tabela e sincronização

--- estagio/estagio/base/sincroniza/sincroniza.py
+++ estagio/estagio/base/sincroniza/sincroniza.py
@@ -5,13 +5,26 @@ from django.conf import settings
 class Sincroniza():
 
     def inicia(self):
-        print('inicio')
+        nome = 'NET_GLOBALI_GLOBALR_PARI_PAR;all_line;T_UR;Tsolo_URsolo;net;globali;globalr;pari;parr;tsoil;ppt;t;rh;u;rhsoil'
+        colunas = nome.split(';')
         arquivos = []
         cont = 1
         for root, dirs, files in os.walk(settings.MEDIA_URL):
             for f in files:
-                arquivos.append({'diretorio': root, 'arquivo':f,'modificado':time.ctime(os.path.getmtime(root + '/' + f)),
-                                 'criado':time.ctime(os.path.getctime(root + '/' + f))})
+                limpo = f.replace('-',' ').replace('_',' ').replace('  ',' ').replace('  ',' ').replace('.csv','')
+                dados = limpo.split(' ')
+                tipo = 'random'
+                if(dados[2] == 'seq'):
+                    tipo = 'sequential'
+                
+                var = {'diretorio': root, 'arquivo':f,'modificado':time.ctime(os.path.getmtime(root + '/' + f)),'criado':time.ctime(os.path.getctime(root + '/' + f)),
+                        'type':dados[2], 
+                        'type_name':tipo,
+                        'percent':int(dados[1]),
+                        'column_name':colunas[int(dados[4])],
+                        'column':int(dados[4])}
+                arquivos.append(var)
+
 
         print('meio')
         from pymongo import MongoClient
